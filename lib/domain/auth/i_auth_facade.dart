6916095999ef1e0ad32ad83ede663b36c7b2403d
ahmetakyyev <akyyev2002@gmail.com>
2023-05-28 15:15:37 +0500

    4th lecture

--- lib/domain/auth/i_auth_facade.dart
+++ lib/domain/auth/i_auth_facade.dart
@@ -5,12 +5,12 @@ import 'package:flutter/foundation.dart';
 
 abstract class IAuthFacade {
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
-    @required EmailAddress emailAddress,
-    @required Password password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
-    @required EmailAddress emailAddress,
-    @required Password password,
+    required EmailAddress emailAddress,
+    required Password password,
   });
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
 }
